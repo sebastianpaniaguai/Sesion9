4e230c5abedfe2fe2284ea3d57602068d2607ab3
sebastianpaniaguai <sebastian.paniaguai@gmail.com>
2015-10-14 16:50:43 -0500

    Initial commit

---
+++ app/src/main/java/com/example/sebas/sesion9/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.sebas.sesion9;
+
+import android.app.FragmentManager;
+import android.support.v4.app.FragmentTabHost;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentTabHost tabHost=(FragmentTabHost) findViewById(R.id.tabhost);
+        tabHost.setup(this, getSupportFragmentManager(),R.id.tabcontent);
+
+        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("Flash"), FlashFragment.class, null);
+        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("Superman"),SupermanFragment.class,null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
