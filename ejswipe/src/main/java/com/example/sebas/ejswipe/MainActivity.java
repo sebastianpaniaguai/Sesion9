2825697f5b0779d5fa60b475fd0d06b2aec07d7a
sebastianpaniaguai <sebastian.paniaguai@gmail.com>
2015-10-15 16:49:36 -0500

    Hola2

---
+++ ejswipe/src/main/java/com/example/sebas/ejswipe/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.sebas.ejswipe;
+
+
+import android.app.FragmentTransaction;
+import android.app.Notification;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements ActionBar.TabListener, ViewPager.OnPageChangeListener {
+    ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager=(ViewPager) findViewById(R.id.pager);
+        PagerAdapter adapter=new PagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        viewPager.setOnPageChangeListener(this);
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        ActionBar.Tab tab = actionBar.newTab().setText("Flash").setTabListener(this);
+        actionBar.addTab(tab);
+
+        tab = actionBar.newTab().setText("Supeman").setTabListener(this);
+        actionBar.addTab(tab);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        getSupportActionBar().setSelectedNavigationItem(position);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction ft) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction ft) {
+
+    }
+
+    public class PagerAdapter extends FragmentPagerAdapter{
+
+        public PagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+         switch (position){
+             case 0:
+                 return new FlashFragment();
+             case 1:
+                 return new SupermanFragment();
+             default:
+                 return null;
+         }
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+    }
+}
